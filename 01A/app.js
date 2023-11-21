50ce605562c7bc335d841c5252ebfc99bdc02219
isaiahajilo <28072@ma-web.nl>
2023-11-21 14:43:40 +0100

    01Apush2

--- 01A/app.js
+++ 01A/app.js
@@ -2,8 +2,21 @@ class App
 {
     runApplication()
     {
+        let Appnaam = "tyrone"
+        let versienummer = 1.0
+        let versiedatum = "21/11/2023"
+        let autheur = "isaiah ajilo"
+        let copyright = "idk"
+        let distributeur = "niemand"
+        let darkmode = true
         console.log("hello world!");
-        //code gaat hier
+        console.log(Appnaam)
+        console.log(versienummer)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
     }
 }
 
