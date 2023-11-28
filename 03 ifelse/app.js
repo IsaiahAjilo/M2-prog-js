629f68aac5a4ebf027220c3ccfbdef3cbc9ab82d
isaiahajilo <28072@ma-web.nl>
2023-11-28 14:26:09 +0100

    03ifelsepush

---
+++ 03 ifelse/app.js
@@ -0,0 +1,23 @@
+class App
+{
+    runApplication()
+    {
+        let versienummer = 1.0
+        let versiedatum = "21/11/2023"
+        let autheur = "isaiah ajilo"
+        let copyright = "idk"
+        let distributeur = "niemand"
+        let darkmode = true
+        console.log("hello world!");
+        console.log(Appnaam)
+        console.log(versienummer)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
+    }
+}
+
+let app = new App();
+app.runApplication();
