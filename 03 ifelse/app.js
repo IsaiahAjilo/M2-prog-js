72089b158254175b3ea701a368149fdd3df84075
isaiahajilo <28072@ma-web.nl>
2023-11-30 14:43:34 +0100

    03ifelsecommit2

--- 03 ifelse/app.js
+++ 03 ifelse/app.js
@@ -2,20 +2,56 @@ class App
 {
     runApplication()
     {
-        let versienummer = 1.0
-        let versiedatum = "21/11/2023"
-        let autheur = "isaiah ajilo"
-        let copyright = "idk"
-        let distributeur = "niemand"
-        let darkmode = true
-        console.log("hello world!");
-        console.log(Appnaam)
-        console.log(versienummer)
-        console.log(versiedatum)
-        console.log(autheur)
-        console.log(copyright)
-        console.log(distributeur)
-        console.log(darkmode)
+      console.log("helloworld!");
+
+      let title = document.getElementById("newstitle");
+      console.log(title);
+      
+      let random = Math.random();
+      console.log(random);
+      if(random < 0.2)
+      {
+        title.style.backgroundColor = "#FF0000";
+      } 
+      else if(random > 0.2 && random <= 0.6)
+      {
+        title.style.backgroundColor = "#FFD700";
+     }
+     else if(random > 0.6 && random <= 0.75)
+     {
+        title.style.backgroundColor = "#00FF00";
+     }
+     else if(random > 0.75) 
+     {
+        title.style.backgroundColor = "#FF69B4";
+     }
+
+     let newsitem1 = document.getElementsByClassName("gamenews")[0]
+     console.log(newsitem1)
+
+     let random1 = Math.random();
+     console.log(random1)
+     if (random1 < 0.5)
+     {
+        newsitem1.style.backgroundColor = "#F00000";
+     }
+     else (random1 > 0.5)
+     {
+        newsitem1.style.backgroundColor = "#00F000";
+     }
+     let newsitem2 = document.getElementsByClassName("gamenews")[1]
+     console.log(newsitem2)
+
+     let random2 = Math.random();
+     console.log(random2)
+     if (random2 < 0.5)
+     {
+        newsitem2.style.backgroundColor = "#00FFFF";
+     }
+     else (random2 > 0.5)
+     {
+        newsitem2.style.backgroundColor = "#F0FF00";
+     }
     }
 }
 
