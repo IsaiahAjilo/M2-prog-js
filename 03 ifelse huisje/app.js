07862c6e897b3d7db99c93f40c129a08dc5f4a61
isaiahajilo <28072@ma-web.nl>
2023-12-05 12:58:43 +0100

    '04functionspush'

---
+++ 03 ifelse huisje/app.js
@@ -0,0 +1,65 @@
+class App
+{
+    runApplicaion()
+    {
+        let Canvas = document.getElementById("CanvasId")
+        let g = Canvas.getContext("2d");
+        g.beginPath();
+        g.fillstyle = "yellow";
+        g.moveTo(30,10);
+        g.lineTo(20,30);
+        g.lineTo(60,40)
+        g.lineTo(70,20)
+        g.closePath();
+        g.stroke();
+        g.fill()
+        
+        g.beginPath();
+        g.fillStyle = "yellow";
+        g.moveTo(20,30);
+        g.lineTo(20,50);
+        g.lineTo(60,60)
+        g.lineTo(60,40)
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath();
+        g.fillstyle = "red";
+        g.moveTo(80,30);
+        g.lineTo(60,40);
+        g.lineTo(60,60)
+        g.lineTo(80,50)
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath();
+        g.fillStyle = "red";
+        g.moveTo(70,20);
+        g.lineTo(60,40);
+        g.lineTo(80,30)
+        g.closePath();
+        g.stroke();
+        g.fill()
+
+        g.beginPath();
+        g.fillStyle = "aqua";
+        g.moveTo(35,50);
+        g.lineTo(40,50);
+        g.lineTo(40,40)
+        g.lineTo(35,40)
+        g.closePath();
+        g.stroke();
+        g.fill()
+        
+        
+        console.log(Canvas)
+        console.log("hello world!")
+        //code
+    }
+
+
+}
+let app = new App();
+app.runApplicaion();
