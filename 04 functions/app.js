4e98fbf59a392ad858e59ac07790c87ca1d2cdf5
isaiahajilo <28072@ma-web.nl>
2023-12-07 14:21:44 +0100

    04functionspush3

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -1,27 +1,35 @@
-function argumentsAreHandy(shoutout){
-    console.log("do you want to give a shoutout");
-    console.log(shoutout);
+function ax2bcWiskunde1(u,h,l){
+
+
+  let logY = Math.log(l) + h + Math.pow(u,2)
+  return logY
+  }
+
+let logY1 = ax2bcWiskunde1(9,8,15)
+console.log(logY1)
+
+function ax2bcWiskunde(x,a,b,c){
+
+
+let y = (a*(x*x) )+ (b*x) +c;
+
+return y
 }
 
-argumentsAreHandy("ello");
-argumentsAreHandy("brood");
-argumentsAreHandy("banaan");
+let y1 = ax2bcWiskunde(9,3,4,89)
+console.log(y1)
+let y2 = ax2bcWiskunde(3,6,5,45)
+console.log(y2)
+let y3 = ax2bcWiskunde(7,1,2,13)
+console.log(y3)
 
-function superMooieGlobalFunction(){
-    console.log("ik ben global");
-    console.log("dus je mag me overal aanroepen");
-}
-
-superMooieGlobalFunction();
-superMooieGlobalFunction();
-superMooieGlobalFunction();
 
 class App
 {
     runApplication()
     {
       console.log("hello world") 
-      superMooieGlobalFunction();
+      
     }
 }
 
