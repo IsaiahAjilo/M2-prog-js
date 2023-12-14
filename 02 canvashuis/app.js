a4ccad54d197143f90a2c05541a91928d0705797
isaiahajilo <28072@ma-web.nl>
2023-12-14 15:19:17 +0100

    02canvashuispush2

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -4,7 +4,11 @@ class App
     {
        let canvas = document.getElementById("canvasId")
        let g = canvas.getContext("2d")
-       g.fillRect(0,0,10,10)
+       g.fillStyle = "#FF0000";
+       g.fillRect(0,0,canvas.width,canvas.height);
+       
+       g.fillStyle = "#000000";
+       g.fillRect(0,0,10,10);               
        console.log(canvas)
     }
 }
